a4086b03e5bdf10b4e4ed8c8c0bb375979aa45f8
Gerardo <gerardmoe@hotmail.com>
2023-08-01 17:25:57 -0500

    backend step 7 and 8

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
 
+morgan.token("data", (req, res) => {
+  const { body } = req;
+
+  return JSON.stringify(body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
