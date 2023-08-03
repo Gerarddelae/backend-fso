5b2dab5956a0dd8bd9eb77da4ed34ab56d7dd8d9
Gerardo <gerardmoe@hotmail.com>
2023-08-03 16:25:17 -0500

    backend step 9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token("data", (req, res) => {
   const { body } = req;
@@ -98,7 +100,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
