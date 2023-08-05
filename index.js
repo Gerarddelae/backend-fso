f75768db5f2fecb12fa6ab6775fb430444999228
Gerardo <gerardmoe@hotmail.com>
2023-08-05 14:38:42 -0500

    added id finder

--- index.js
+++ index.js
@@ -70,14 +70,18 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(400).end()
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(400).send({ error: 'malformatted id'})
+  })
 });
 
 app.delete("/api/persons/:id", (request, response) => {
