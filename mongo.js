e8d8e90536d0ffc9dd0a07bd68c8b158cb764040
Gerardo <gerardmoe@hotmail.com>
2023-08-05 13:33:22 -0500

    added mongo.js

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.ypxo6ve.mongodb.net/?retryWrites=true&w=majority`
+  
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 4) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+    return
+}
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
